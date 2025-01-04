53ebbd37e2a46da2eeb422384a8b9816a2ff3140
noglitches <1998010700425@ofppt-edu.ma>
2025-01-04 09:50:34 +0100

    Added tailwind Sonar logic for testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import './App.css'
+import Header from './components/Header.jsx'
 
 function App() {
 
   return (
     <>
-      <h1>
-        Hello actions!
-      </h1>
+      <Header />
     </>
   )
 }
