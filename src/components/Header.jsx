62f079ad8b3bf5a0d01be54ce7e84a41ea08901d
noglitches <1998010700425@ofppt-edu.ma>
2025-01-04 11:32:15 +0100

    some other changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -166,7 +166,7 @@ export default function Example() {
                   href="#"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                 >
-                  Features
+                  Projects
                 </a>
                 <a
                   href="#"
