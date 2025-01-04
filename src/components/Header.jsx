02ce93119c8a9f923e5d54ea29b58f05d159a485
noglitches <1998010700425@ofppt-edu.ma>
2025-01-04 11:41:14 +0100

    small change to trigger testing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -172,7 +172,7 @@ export default function Example() {
                   href="#"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                 >
-                  Marketplace
+                  Members
                 </a>
                 <a
                   href="#"
